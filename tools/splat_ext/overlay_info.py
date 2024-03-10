3d2ceb9783ddba8d684b50909d2216a6c2427157
Angie <angheloalf95@gmail.com>
2024-03-09 22:57:12 -0300

    Name files for init and ctor files

--- tools/splat_ext/overlay_info.py
+++ tools/splat_ext/overlay_info.py
@@ -10,3 +10,6 @@ from splat.segtypes.common.rodata import CommonSegRodata
 class PS2SegOverlay_info(CommonSegRodata):
     def get_linker_section(self) -> str:
         return ".overlay_info"
+
+    def get_section_flags(self) -> str|None:
+        return "a"
--- tools/splat_ext/vtables.py
+++ tools/splat_ext/vtables.py
@@ -10,3 +10,6 @@ from splat.segtypes.common.rodata import CommonSegRodata
 class PS2SegVtables(CommonSegRodata):
     def get_linker_section(self) -> str:
         return ".vtables"
+
+    def get_section_flags(self) -> str|None:
+        return "a"
