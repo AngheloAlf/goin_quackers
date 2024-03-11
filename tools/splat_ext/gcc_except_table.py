f29c3ec7cdfef70c2f6fb0f55229eb1c567d71c3
angie <angheloalf95@gmail.com>
2024-03-11 13:46:17 -0300

    gcc_except_table

--- tools/splat_ext/gcc_except_table.py
+++ tools/splat_ext/gcc_except_table.py
@@ -7,9 +7,9 @@ from __future__ import annotations
 
 from splat.segtypes.common.rodata import CommonSegRodata
 
-class PS2SegOverlay_info(CommonSegRodata):
+class PS2SegGcc_except_table(CommonSegRodata):
     def get_linker_section(self) -> str:
-        return ".overlay_info"
+        return ".gcc_except_table"
 
     def get_section_flags(self) -> str|None:
         return "a"
